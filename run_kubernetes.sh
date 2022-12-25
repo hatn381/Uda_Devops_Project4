74c934eae345e8fae6ec05d91704c380ac8682a8
Le Tan Hai <tanhai@MacBook-Pro.local>
2022-12-25 18:24:04 +0700

    update code of final

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath=hatn5/udacitydevops:latest
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+# kubectl create deploy devop-project4-app --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward pod/devop-project4-app-654666748d-r9478 --address 0.0.0.0 8080:80
 
+# Logs
+kubectl logs devop-project4-app-654666748d-r9478
